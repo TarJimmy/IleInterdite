b1ac4b0193643e2526dc5a36b02079de2cf0a4a2
gholbin <35114336+gholbin@users.noreply.github.com>
2019-05-30 22:00:56 +0200

    majorité des fonctions
    la majorité des fonctions ont été codées

--- src/Modele/Aventurier.java
+++ src/Modele/Aventurier.java
@@ -1,109 +1,132 @@
 package Modele;
 
-import Modele.Utils.tresor;
+import Vue.MesEnums.tresor;
 import Vue.MesEnums.typeAction;
+
 import java.util.*;
 
 public abstract class Aventurier {
 
-	private Tuile maPos;
-	Collection<CarteJoueur> mesCartes;
-	private ArrayList<tresor> mesTresors;
-	private int actionsRestantes;
+    private Tuile maPos;
+    ArrayList<CarteJoueur> mesCartes;
+    private int actionsRestantes;
+    private Pion pion;
+    private static HashSet<tresor> tresorsRecuperer = new HashSet<>();
 
-	/**
-	 * 
-	 * @param tuile
-	 */
-	public void deplacer(Tuile tuile) {
-		// TODO - implement Aventurier.deplacer
-		throw new UnsupportedOperationException();
-	}
 
-	/**
-	 * 
-	 * @param Tuile
-	 */
-	public void assecher(Tuile Tuile) {
-		// TODO - implement Aventurier.assecher
-		throw new UnsupportedOperationException();
-	}
+    public void deplacer(Tuile tuile) {
+        setMaPos(tuile);
+    }
 
-	public void donnerCarte() {
-		// TODO - implement Aventurier.donnerCarte
-		throw new UnsupportedOperationException();
-	}
+    public static HashSet<tresor> getTresorsRecuperer() {
+        return tresorsRecuperer;
+    }
 
-	public void GagnerTresor() {
-		// TODO - implement Aventurier.GagnerTresor
-		throw new UnsupportedOperationException();
-	}
 
-	public ArrayList<typeAction> getActionsPossibles() {
-		// TODO - implement Aventurier.getActionsPossibles
-		throw new UnsupportedOperationException();
-	}
+    public void assecher(Tuile tuile) {
+    tuile.assecher();
+    actionsRestantes--;
+     }
 
-	public ArrayList<CarteJoueur> getCartes() {
-		// TODO - implement Aventurier.getCartes
-		throw new UnsupportedOperationException();
-	}
+    public void DonnerCarte(CarteJoueur carte, Aventurier av) {
+        av.AddCarte(carte);
+        SupprimerCarte(carte);
+        actionsRestantes--;
+    }
 
-	/**
-	 * 
-	 * @param carte
-	 * @param Av
-	 */
-	public void DonnerCarte(CarteJoueur carte, Aventurier Av) {
-		// TODO - implement Aventurier.DonnerCarte
-		throw new UnsupportedOperationException();
-	}
 
-	/**
-	 * 
-	 * @param carte
-	 */
-	public void SupprimerCarte(CarteJoueur carte) {
-		// TODO - implement Aventurier.SupprimerCarte
-		throw new UnsupportedOperationException();
-	}
+    public boolean GagnerTresor(tresor tres) {
+            if(getTresorsRecuperer().contains(tres)){
+                int nbRetirés = 0;
+                Iterator it = getCartes().iterator();
+                while(it.hasNext()){
+                    CarteJoueur carte =(CarteJoueur) it.next();
+                    if(carte instanceof CarteTresor){
+                        if( carte.getTresor() == tres)
+                        it.remove();
+                        nbRetirés++;
+                    }
+                }
+            }
+    }
 
-	/**
-	 * 
-	 * @param carte
-	 */
-	public void AddCarte(CarteJoueur carte) {
-		// TODO - implement Aventurier.AddCarte
-		throw new UnsupportedOperationException();
-	}
+    public ArrayList<CarteJoueur> getCartes() {
+        return mesCartes;
+    }
 
-	public Tuile getTuile() {
-		// TODO - implement Aventurier.getTuile
-		throw new UnsupportedOperationException();
-	}
 
-	/**
-	 * 
-	 * @param tres
-	 */
-	public void addTresor(tresor tres) {
-		// TODO - implement Aventurier.addTresor
-		throw new UnsupportedOperationException();
-	}
+    public void SupprimerCarte(CarteJoueur carte) {
+        getCartes().remove(carte);
+    }
 
-	public int nbCarte() {
-		// TODO - implement Aventurier.nbCarte
-		throw new UnsupportedOperationException();
-	}
+    public void AddCarte(CarteJoueur carte) {
+        getCartes().add(carte);
+    }
 
-	public boolean checkDeplacement() {
-		// TODO - implement Aventurier.checkDeplacement
-		throw new UnsupportedOperationException();
-	}
+    public Tuile getTuile() {
+        return maPos;
+    }
 
-	public ArrayList<Tuile> getDeplacement() {
-		// TODO - implement Aventurier.getDeplacement
-		throw new UnsupportedOperationException();
-	}
+    public int nbCarte() {
+        return getCartes().size();
+    }
 
+    public boolean checkDeplacement(Tuile tuile) {
+        return tuile.estDisponible();
+    }
+
+    public ArrayList<Tuile> getDeplacement(Grille grille) {
+        grille.getVoisins(getTuile(),getCoordsProche());
+        //TO DO
+        return null;
+    }
+
+    public void DebutTour() {
+        setActionsRestantes(3);
+    }
+
+    public void Piocher(DeckTresor deck) {
+        // TODO - implement Aventurier.Piocher
+        throw new UnsupportedOperationException();
+    }
+
+    public ArrayList<int[]> getCoordsProche() {
+        ArrayList<int[]> coords = new ArrayList<>();
+        int d;
+        for(int x = -1;x<=1;x++){
+            for(int y = -1;y <= 1;y++){
+                d = Math.abs(x+y);
+                coords.add((d == 1)? new int[] {x,y} :null);
+            }
+        }
+        return coords;
+    }
+
+
+
+    public ArrayList<Tuile> getAssechement(Grille grille) {
+        // TODO - implement Aventurier.getAssechement
+        throw new UnsupportedOperationException();
+    }
+
+
+    public boolean checkAssechement(Tuile tui) {
+        return tui.isInnondee();
+    }
+
+    public Tuile getMaPos() {
+        return maPos;
+    }
+
+    private void setMaPos(Tuile maPos) {
+        this.maPos = maPos;
+    }
+
+    public int getActionsRestantes() {
+        return actionsRestantes;
+    }
+
+    private void setActionsRestantes(int actionsRestantes) {
+        this.actionsRestantes = actionsRestantes;
+    }
 }
