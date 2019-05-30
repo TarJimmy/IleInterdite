bd5c6de3bceffaf6fa81ee646fc24c49cf75c268
gholbin <35114336+gholbin@users.noreply.github.com>
2019-05-30 23:13:33 +0200

    Override de l'ingenieur
    les fonctions que l'ingenieur Override ont été ajoutés

--- src/Modele/Ingenieur.java
+++ src/Modele/Ingenieur.java
@@ -4,15 +4,49 @@ import Modele.Utils.Pion;
 
 public class Ingenieur extends Aventurier {
 
-	private Pion pionRouge;
+    private boolean aAssecher;
 
-	/**
-	 * 
-	 * @param Tuile
-	 */
-	public void assecher(Tuile Tuile) {
-		// TODO - implement Ingenieur.assecher
-		throw new UnsupportedOperationException();
-	}
+    public void setaAssecher(boolean aAssecher) {
+        this.aAssecher = aAssecher;
+    }
+    
+    public boolean aAssecher() {
+        return aAssecher;
+    }
+    @Override
+    public void assecher(Tuile Tuile) {
+        super.assecher(Tuile);
+        if(aAssecher()){
+            setaAssecher(false);
+            actionsRestantes++;
+        }else{
+            setaAssecher(true);
+        }
+        
+    }
+
+    @Override
+    public boolean GagnerTresor(Utils.tresor tres) {
+        setaAssecher(false);
+        return super.GagnerTresor(tres);
+    }
+    
+    @Override
+    public void DonnerCarte(CarteJoueur carte, Aventurier Av) {
+        super.DonnerCarte(carte, Av);
+        setaAssecher(false);
+    }
+
+    @Override
+    public void deplacer(Tuile tuile) {
+        super.deplacer(tuile);
+        setaAssecher(false);
+    }
+    
+    @Override
+    public void DebutTour() {
+        super.DebutTour();
+        setaAssecher(false);
+    }
 
 }
