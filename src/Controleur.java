1ecbf32b81ade2e1512a5eddf0c69f2c7f9f98d4
TarJimmy <50171737+TarJimmy@users.noreply.github.com>
2019-06-01 17:54:43 +0200

    Add files via upload

--- src/Controleur.java
+++ src/Controleur.java
@@ -5,15 +5,72 @@
  */
 package Controleur;
 
+import Modele.*;
+import Vue.*;
+import java.util.ArrayList;
+import java.util.Observable;
+
 /**
  *
  * @author tardy
  */
-public class Controleur implements Observateur{
+public class Controleur implements Observateur {
+    private VueAccueil accueil;
+    private VueGrille vueGrille;
+    private VueJeu vueJeu;
+    private ArrayList<Vue.VueAventurier> mesVuesAventuriers;
+    private ArrayList<Aventurier> mesAventuriers;
+    private Grille grille;
+    private DeckInnondation deckInnondation;
+    private DeckTresor deckTresor;
+    
+    Controleur(){
+        accueil = new VueAccueil();
+        accueil.addObservateur(this);
+        accueil.afficher(true);
+    }
+    public void piocher (Aventurier av){
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    public boolean partieGagne(){
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    public void TourDeJeu(){
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    public Aventurier getTourActuelAventurier(){
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    public boolean partiePerdu(){
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    public void FaireAction(String Action){
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    public void MonteeDesEaux(){
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    public void faireDefausser(Aventurier av){
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    public void finTour(){
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    public void CheckNbCarte(){
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    public void CheckMonteeEau(){
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    
 
     @Override
     public void traiterMessage(Message msg) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        switch(msg.type){
+            case DEBUTJEU:
+                grille= new Grille(msg.difficulte);
+                
+        }
     }
-    
 }
