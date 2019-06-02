6fe19f84cd92800a5c3d119688ce024048942449
TarJimmy <50171737+TarJimmy@users.noreply.github.com>
2019-06-02 16:22:54 +0200

    Add files via upload

--- src/Controleur.java
+++ src/Controleur.java
@@ -77,20 +77,28 @@ public class Controleur implements Observateur {
         mesAvs.add(new Explorateur());
         mesAvs.add(new Plongeur());
         mesAvs.add(new Pilote());
+        System.out.println("Créé un ArrayList d'aventurier : "+ mesAvs);
+        System.out.println("Supprime "+(6-nbAventuriers)+" aventuriers de la liste pour avoir une taille de "+nbAventuriers+" Aventuriers");
         for (int i=0;i<(6-nbAventuriers);i++){
             int r1= (int) (Math.random() * (6-i));
+            System.out.println("Supprime "+ mesAvs.get(r1).getNomAventurier() + " ( n°"+ r1+ " de la liste)");
             mesAvs.remove(mesAvs.get(r1));
         }
+        System.out.println("La liste contient : "+ mesAvs);
+        
         return mesAvs;
     }
     private ArrayList<VueAventurier> creationVuesAventuriers(ArrayList<Aventurier> mesAvs, ArrayList<String> mesNoms){
         ArrayList<VueAventurier> mesVues = new ArrayList<>();
+        System.out.println("Créé une liste de VueAventurier a partir celui des Aventuriers");
         int taille = mesAvs.size();
-        System.out.println("hey1");
         for (int i =0;i<taille;i++){
             mesVues.add(new VueAventurier(mesNoms.get(i),mesAvs.get(i).getNomAventurier(),mesAvs.get(i).getPion().getCouleur()));
-            System.out.println("hey");
+            System.out.println("Crée une VueAventurier avec pour parametre : ["+mesNoms.get(i)+"]"
+                                                                               + "["+ mesAvs.get(i).getNomAventurier()+"]"
+                                                                               + "["+ mesAvs.get(i).getPion().toString()+"]");
         }
+        System.out.println("Liste Finale : " + mesVues);
         return mesVues;
     }
     
@@ -99,12 +107,19 @@ public class Controleur implements Observateur {
     public void traiterMessage(Message msg) {
         switch(msg.type){
             case DEBUTJEU:
+                System.out.println("------------Desactive L'accueil---------");
                 accueil.afficher(false);
+                System.out.println("------------Creation de la Grille-------");
                 grille= new Grille(3);
+                System.out.println("-----------Création de la VueGrille-----");
                 vueGrille = new VueGrille(grille);
+                System.out.println("----------Creations des Aventuriers-----");
                 mesAventuriers = creationAventurier(msg.nbJoueur);
+                System.out.println("----------Création des VuesAventuriers--");
                 mesVuesAventuriers = creationVuesAventuriers(mesAventuriers,msg.noms);
+                System.out.println("------------Création de la VueJeu-------");
                 jeu = new VueJeu(vueGrille,mesVuesAventuriers);
+                System.out.println("--------------Affiche la VueJeu---------");
                 jeu.afficher(true);
         }
     }
