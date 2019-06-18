62085ba50bfc1a3eb718c772648e4cd795111662
Beaufima <50876160+Beaufima@users.noreply.github.com>
2019-06-18 10:43:18 +0200

    Update VueCarte.java

--- src/Vue/VueCarte.java
+++ src/Vue/VueCarte.java
@@ -1,4 +1,16 @@
 package Vue;
 
+import java.awt.Image;
+import static ile_interdite.CarteUtils.*;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
 public class VueCarte {
+  
+  VueCarte(CarteUtils carte){
+    JLabel l = new JLabel();
+    l.setIcon(carte.getImage());
+       
+    add(l);
+  }
 }
