2f57afcb2b521ff09799f49832783b8b0031b006
Beaufima <50876160+Beaufima@users.noreply.github.com>
2019-06-18 14:44:42 +0200

    Update VueCarte.java

--- src/Vue/VueCarte.java
+++ src/Vue/VueCarte.java
@@ -1,12 +1,19 @@
 package Vue;
 
 import java.awt.Image;
-import static Vue.CarteUtils.*;
+import Vue.CarteUtils;
 import java.io.IOException;
+import javax.swing.JFrame;
 
 public class VueCarte extends PanelImage {
   
     VueCarte(CarteUtils carte) throws IOException{
         super(carte.getChemin());
     }
+      /* public static void main(String[]ntmMalo) throws IOException{
+        JFrame j = new JFrame("Test");
+        j.setSize(300, 200);
+        j.add(new VueCarte(CarteUtils.));
+        j.setVisible(true);
+    }*/
 }
